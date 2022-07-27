1cbc9d1f83388941e64bac6bf027b28223ea6455
Si-saber Sidali <88353362+Siiidali@users.noreply.github.com>
2022-07-27 13:24:58 +0200

    making chat messages

--- frontend/src/components/Chat.jsx
+++ frontend/src/components/Chat.jsx
@@ -1,7 +1,44 @@
-function Chat() {
+import { useState } from 'react';
+import {useGameContext} from '../hooks/useGameContext';
+import {usePlayerContext} from '../hooks/usePlayerContext';
+
+
+function Chat({socket}) {
+    const {messages,setMessages} = useGameContext();
+    const [newMessage,setNewMessage] = useState('')
+    const {code,name} = usePlayerContext();
+
+    socket.on('message',(message,name)=>{
+        setMessages([...messages,{name,message}]);
+    })
+
+    const changeHandler = (e)=>{
+        setNewMessage(e.target.value);
+    }
+
+    const submitHandler = (e)=>{
+        e.preventDefault();
+        socket.emit('message' , code , newMessage, name)
+        setMessages([...messages,{name:`${name} (YOU)`,message : newMessage}]);
+        console.log(messages);
+    }
+
+    let i = 0 ;
     return ( 
-        <div>
-            chat
+        <div className="chat-container">
+           <div className='messages'>
+                {messages && messages.map(message=>{
+                    return (
+                        <div key={i++} className='message'>
+                            <h3>{message.name}</h3>
+                            <p>{message.message}</p>
+                        </div>
+                    );
+                })}
+           </div>
+           <form onSubmit={submitHandler}>
+                <input type="text" onChange={changeHandler} value={newMessage}/>
+           </form>
         </div>
      );
 }
